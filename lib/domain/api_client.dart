0bc7cb2a8f8fd5afad2d60eada3b41d81aa28f00
ApolloNTZX <8112733+iBelow@users.noreply.github.com>
2023-07-21 14:00:18 +0300

    updated comments

--- lib/domain/api_client.dart
+++ lib/domain/api_client.dart
@@ -12,8 +12,10 @@ abstract class IReddit {
 
 class RedditImplementation implements IReddit {
   static String apiUrl = 'https://www.reddit.com/r/flutterdev/new.json';
+  // кастмный клиент (если нужно)
   final http.Client _client;
 
+  // конструктор, который создаёт клиент, если его не передавать
   RedditImplementation({http.Client? client}) : _client = client ?? http.Client();
 
   @override
@@ -21,30 +23,39 @@ class RedditImplementation implements IReddit {
     try {
       final data$response = await _client.get(Uri.parse(RedditImplementation.apiUrl));
       if (data$response.statusCode == 200) {
-        //& simple for example
-        //& get data:
+        //& Простой пример
+        //& Получаем данные:
+        //& по факту, здесь строка конвертится в Map<String,Object>
+        // так как это Map, мы можем перейти по ключу [data][children], как в JSON с сервера
         final childLst = json.decode(utf8.decode(data$response.bodyBytes))['data']['children'];
-        //& set model
+        //& Создаем модель (объекты GetChildrenObject)
         return List<GetChildrenObject>.from(childLst.map((e) => GetChildrenObject.fromJson(e)));
-        //^ or simple:
+        //^ Или вариант проще (одной строкой фактически):
         // return List<GetChildrenObject>.from(json
         //     .decode(utf8.decode(data$response.bodyBytes))['data']['children']
         //     .map((e) => GetChildrenObject.fromJson(e)));
       } else {
+        // Если ответ от сервера не имеет статус 200 (успешный ответ),
+        // выбрасываем исключение (Exception).
+        // Позже, возможно, стоит заменить на собственное исключение.
         throw Exception();
-
-        ///change to custom exception
       }
     } on SocketException catch (error, stackTrace) {
+      // Если возникнет ошибка сокета (например, нет интернет-соединения),
+      // то обработаем её здесь, выводим информацию об ошибке и пробрасываем дальше.
       Error.safeToString(error);
       stackTrace.toString();
       rethrow;
     } on Object catch (error, stackTrace) {
+      // Если произойдет какая-либо другая ошибка, которую мы не предвидим,
+      // то также обработаем её здесь, выводим информацию об ошибке и пробрасываем дальше.
       Error.safeToString(error);
       stackTrace.toString();
       rethrow;
     } finally {
-      // ...
+      // Здесь можно добавить код, который выполнится независимо от того, произошла ли ошибка или нет.
+      // Например, можно закрыть соединение, если оно было открыто ранее.
+      // Конкретно тут нечего чистить, обрабатывать, и пр
     }
   }
 
